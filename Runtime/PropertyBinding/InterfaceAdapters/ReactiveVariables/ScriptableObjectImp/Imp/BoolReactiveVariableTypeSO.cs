14a7efea240b06bc811c548656d36c62eaeffccc
Lalo <laurencioberro@hotmail.com>
2022-11-02 17:28:19 -0300

    [A] Add float and bool reactive variables

---
+++ Runtime/PropertyBinding/InterfaceAdapters/ReactiveVariables/ScriptableObjectImp/Imp/BoolReactiveVariableTypeSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace MVVM.PropertyBinding.InteraceAdapters
+{
+    [CreateAssetMenu(fileName = "ReactiveVariableSO", menuName = "ScriptableObjects/MVVM/Variables/Bool", order = 0)]
+    public class BoolReactiveVariableTypeSO : ReactiveVariableTypeSO<bool>
+    {
+
+    }
+}
---
+++ Runtime/PropertyBinding/InterfaceAdapters/ReactiveVariables/ScriptableObjectImp/Imp/FloatReactiveVariableTypeSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace MVVM.PropertyBinding.InteraceAdapters
+{
+    [CreateAssetMenu(fileName = "ReactiveVariableSO", menuName = "ScriptableObjects/MVVM/Variables/Float", order = 0)]
+    public class FloatReactiveVariableTypeSO : ReactiveVariableTypeSO<float>
+    {
+
+    }
+}
